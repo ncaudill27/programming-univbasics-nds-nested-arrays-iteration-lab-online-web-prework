3ffbf50c74d09b1aac00289c70129bd77493a81d
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-18 20:18:40 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,17 +1,33 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  outer_array = []
+  food_pair = 0
+  while food_pair < src.count do
+    inner_array = []
+    food_item = 0
+      while food_item < src[food_pair].count do
+        inner_array << src[food_pair][food_item]
+        food_item += 1
+      end
+    outer_array << "I love #{inner_array[0]} and #{inner_array[1]} on my pizza"
+    food_pair += 1
+  end
+  outer_array
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  greater_numbers = []
+  row_index = 0
+  while row_index < src.count do
+    first_number = src[row_index][0]
+    second_number = src[row_index][1]
+    if first_number > second_number
+      greater_numbers << first_number
+    else
+      greater_numbers << second_number
+    end
+    row_index += 1
+  end
+  greater_numbers
 end
 
 def total_even_pairs(src)
@@ -22,4 +38,12 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  even_pairs = []
+  sum_even_pairs = 0
+  row_index = 0
+  while row_index < src.count  do
+    p src[row_index]
+  end
+  row_index += 1
+  p even_pairs
 end
