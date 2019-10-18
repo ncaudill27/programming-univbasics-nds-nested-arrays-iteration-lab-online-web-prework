fc2db28755a4af17185c39d7b1c9787af5816142
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-18 20:48:12 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,43 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  outer_array = []
+  food_pair = 0
+  while food_pair < src.count do
+    inner_array = []
+    food_item = 0
+      while food_item < src[food_pair].count do
+        inner_array << src[food_pair][food_item]
+        food_item += 1
+      end
+    outer_array << "I love #{inner_array[0]} and #{inner_array[1]} on my pizza"
+    food_pair += 1
+  end
+  outer_array
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  greater_numbers = []
+  row_index = 0
+  while row_index < src.count do
+    first_number = src[row_index][0]
+    second_number = src[row_index][1]
+    if first_number > second_number
+      greater_numbers << first_number
+    else
+      greater_numbers << second_number
+    end
+    row_index += 1
+  end
+  greater_numbers
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  even_pairs = []
+  row_index = 0
+  while row_index < src.count do
+    if src[row_index].sum % 2 == 0 and src[row_index][0] % 2 == 0
+      even_pairs << src[row_index].sum
+    end
+    row_index += 1
+  end
+  sum_of_even_pairs = even_pairs.sum
 end
